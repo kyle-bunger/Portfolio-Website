9b44ef6ff77227c0baabc88eba36202788d53a6c
Kyle Bunger <kbunger@ramquest.com>
2023-02-25 22:45:17 -0600

    Work in progress

---
+++ MVC_PortfolioWebsite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC_PortfolioWebsite.Models;
+
+namespace MVC_PortfolioWebsite.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult AboutMe()
+    {
+        return View();
+    }
+
+    public IActionResult Connect()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ MVC_PortfolioWebsite/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const changeTheme = document.querySelector('#dark-mode-toggle');
+changeTheme.addEventListener("click", function () {
+    const everything = document.querySelectorAll('*');
+    everything.forEach((item) => {
+        item.classList.toggle('dark');
+    });
+});
